24a9595d20dd986f8526fb5f8a95c062b6936ac0
zzupd@naver.com <zzupd@naver.com>
2021-04-09 14:05:12 +0900

    0409_엔터이벤트를 적용한 로그인 실행

--- script/script.js
+++ script/script.js
@@ -1,26 +1,72 @@
-$(function(){
+$(function () {
 
-    $("button").click(function(){
+
+    document.onkeyup = fnEnterChk;
+
+    function fnEnterChk() {
+        var keycode = window.event.keyCode;
+        if (keycode == 13) {
+            fnSubmit();
+        }
+    }
+
+    function fnSubmit() {
+        /*alert("서브밋 OK!");*/
+
+
         var uID = $("#uID").val().trim();
         // 입력된 ID value 가져오기 및 공백제거(메서드 체인 적용)
         var uPW = $("#uPW").val().trim();
         var regExpID = /[^a-z|A-Z|0-9|_]/;
-                 // ID 정규표현식 유효성검사조건
+        // ID 정규표현식 유효성검사조건
         var regExpPW = /[^a-z|A-Z|0-9|_#]/;
-                 // PW 정규표현식 유효성검사조건
+        // PW 정규표현식 유효성검사조건
 
-        if (uID == ""   ||    regExpID.test(uID)) {
+        if (uID == "" || regExpID.test(uID)) {
             alert("아이디를 올바르게 입력해주세요.");
-            $("#uID").focus().val("");   // 포커스설정 및 공백제거
+            $("#uID").focus().val(""); // 포커스설정 및 공백제거
 
         } else if (uPW == "" || regExpPW.test(uPW)) {
             alert("비밀번호를 올바르게 입력해주세요.");
             $("#uPW").focus().val("");
 
         } else {
-                alert("OK!");   // ID, PW 모두 정상적인 값이 입력됨
+            alert("OK!"); // ID, PW 모두 정상적인 값이 입력됨
 
         }
+
+    }
+
+
+
+
+
+
+
+    $("button").click(function () {
+
+
+        var uID = $("#uID").val().trim();
+        // 입력된 ID value 가져오기 및 공백제거(메서드 체인 적용)
+        var uPW = $("#uPW").val().trim();
+        var regExpID = /[^a-z|A-Z|0-9|_]/;
+        // ID 정규표현식 유효성검사조건
+        var regExpPW = /[^a-z|A-Z|0-9|_#]/;
+        // PW 정규표현식 유효성검사조건
+
+        if (uID == "" || regExpID.test(uID)) {
+            alert("아이디를 올바르게 입력해주세요.");
+            $("#uID").focus().val(""); // 포커스설정 및 공백제거
+
+        } else if (uPW == "" || regExpPW.test(uPW)) {
+            alert("비밀번호를 올바르게 입력해주세요.");
+            $("#uPW").focus().val("");
+
+        } else {
+            alert("OK!"); // ID, PW 모두 정상적인 값이 입력됨
+
+        }
+
 
     });
 
